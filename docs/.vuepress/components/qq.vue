b3ceb239f4d0d8fe698242961c33719a572f9bb5
liweiwh <liweiwh@itcast.cn>
2019-09-04 15:14:33 +0800

    techs首页介绍

---
+++ docs/.vuepress/components/qq.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <a href="javascript:void(0)" class="hover-img" @click="show()">512353510</a>(点击展开)
+    <div>
+      <img v-if="isShow" src="~@static/qq.jpg" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        isShow: false
+      }
+    },
+    methods: {
+      show () {
+        this.isShow = !this.isShow
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: inline;
+}
+</style>
